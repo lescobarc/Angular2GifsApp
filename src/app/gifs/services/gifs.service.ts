db0132712fe63c685d4aec072a55dac6870d4d74
lauraescobarc <66136705+lescobarc@users.noreply.github.com>
2021-05-05 20:45:08 -0500

    S7-84 Mostrar los resultados en pantalla

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -9,6 +9,8 @@ export class GifsService {
   private apiKey: string = 'B2Mo4B0m83BAtxgMYgl70bKCh5vb4JHb';
   private _historial: string[] = [];
 
+  public resultados: any[] = [];
+
   get historial() {
     //this._historial = this._historial.splice(0,10)
     return [...this._historial]
@@ -23,9 +25,10 @@ export class GifsService {
       this._historial = this._historial.splice(0, 10)
     }
 
-    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=B2Mo4B0m83BAtxgMYgl70bKCh5vb4JHb&q=cat&limit=10')
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=B2Mo4B0m83BAtxgMYgl70bKCh5vb4JHb&q=${query}&limit=10`)
       .subscribe((resp: any) =>{
         console.log(resp.data);
+        this.resultados = resp.data;
       })
 
   }
