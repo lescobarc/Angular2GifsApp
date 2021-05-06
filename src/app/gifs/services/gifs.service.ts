fa4108ebd13f508b600062f2649b1a12016c769c
lauraescobarc <66136705+lescobarc@users.noreply.github.com>
2021-05-05 20:27:01 -0500

    S7-83 Realizar una petición HTTP

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,25 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
-  private apiKey : string = 'B2Mo4B0m83BAtxgMYgl70bKCh5vb4JHb';
+  private apiKey: string = 'B2Mo4B0m83BAtxgMYgl70bKCh5vb4JHb';
   private _historial: string[] = [];
 
-  get historial(){
+  get historial() {
     //this._historial = this._historial.splice(0,10)
     return [...this._historial]
   }
 
-  buscarGifs (query: string=''){
+  constructor(private http: HttpClient) { }
+
+  buscarGifs(query: string = '') {
     query = query.trim().toLowerCase();
-    if(!this._historial.includes(query)){
+    if (!this._historial.includes(query)) {
       this._historial.unshift(query);
-      this._historial = this._historial.splice(0,10)
+      this._historial = this._historial.splice(0, 10)
     }
-    
-    console.log(this._historial)
+
+    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=B2Mo4B0m83BAtxgMYgl70bKCh5vb4JHb&q=cat&limit=10')
+      .subscribe((resp: any) =>{
+        console.log(resp.data);
+      })
+
   }
 }
