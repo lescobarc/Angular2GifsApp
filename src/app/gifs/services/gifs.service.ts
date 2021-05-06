bfa167175d7bdfd96d5aca3a2fee6c322d3d68dc
lauraescobarc <66136705+lescobarc@users.noreply.github.com>
2021-05-05 20:59:50 -0500

    S7-85 Colocando un tipado a las peticiones http

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Gif, SearchGifsResponse } from './../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class GifsService {
   private apiKey: string = 'B2Mo4B0m83BAtxgMYgl70bKCh5vb4JHb';
   private _historial: string[] = [];
 
-  public resultados: any[] = [];
+  public resultados: Gif[] = [];
 
   get historial() {
     //this._historial = this._historial.splice(0,10)
@@ -25,7 +26,7 @@ export class GifsService {
       this._historial = this._historial.splice(0, 10)
     }
 
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=B2Mo4B0m83BAtxgMYgl70bKCh5vb4JHb&q=${query}&limit=10`)
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=B2Mo4B0m83BAtxgMYgl70bKCh5vb4JHb&q=${query}&limit=10`)
       .subscribe((resp: any) =>{
         console.log(resp.data);
         this.resultados = resp.data;
