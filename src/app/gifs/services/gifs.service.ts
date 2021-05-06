94cf252e09ae450c44097259187f71ef698537b9
lauraescobarc <66136705+lescobarc@users.noreply.github.com>
2021-05-05 21:16:07 -0500

    S7-86 LocalStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule, JsonpClientBackend } from '@angular/common/http';
 import { Gif, SearchGifsResponse } from './../interface/gifs.interface';
 
 @Injectable({
@@ -17,13 +17,21 @@ export class GifsService {
     return [...this._historial]
   }
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || []
+   /*  if(localStorage.getItem('historial')){
+      this._historial = JSON.parse(localStorage.getItem('historial')!); 
+    } */
+   }
 
   buscarGifs(query: string = '') {
     query = query.trim().toLowerCase();
     if (!this._historial.includes(query)) {
       this._historial.unshift(query);
       this._historial = this._historial.splice(0, 10)
+
+      //Grabar Localstorage
+      localStorage.setItem('historial', JSON.stringify(this._historial))
     }
 
     this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=B2Mo4B0m83BAtxgMYgl70bKCh5vb4JHb&q=${query}&limit=10`)
